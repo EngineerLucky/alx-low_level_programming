3cba8373a3a7ec72c5ff08b1b724c12959cc1584
Lucksen <lucksenhlatshwayo19@gmail.com>
2023-02-16 17:43:12 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,18 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
- * main -Entry point
+ * main - A program thats prints a line using printf function
+ * the computer is compiled and run on..
  * Return: Always 0 (Success)
  */
 
 int main(void)
+
 {
-	Printf("Size of a char: % zu byte(s)\n", sizeof(char));
-	Printf("Size of an int: % zu byte(s)\n", sizeof(int));
-	Printf("Size of a long int: % zu byte(s)\n", sizeof(long int));
-	Printf("Size of a long long int: % zu byte(s)\n", sizeof(long long int));
-	Printf("Size of a float: % zu byte(s)\n", sizeof(float));
+	printf("Size of a char: %zu bytes(s)\n", sizeof(char));
+	printf("Size of an int: %zu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %zu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu bytes(s)\n", sizeof (long long int));
+	printf("Size of a float: %zu bytes(s)\n", sizeof(afloat));
 	return (0);
 }
