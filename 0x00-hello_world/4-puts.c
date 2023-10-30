ece63e4f7327ecc8291805ca9096aade785bcbaa
EngineerLucky <lucksehlatshwayo19@gmail.com>
2023-10-30 12:46:42 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - It should print out exactly
+ * Programming is like building a multilingual puzzle
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main -It should have  Entry point
+ *
+ * Description: A  program that prints with printf function.
+ *
+ * Return: Always 0  (Sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
