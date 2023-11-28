5e04f90932aac6955f1f1b98e05c111a60189faf
EngineerLucky <lucksehlatshwayo19@gmail.com>
2023-11-27 19:58:03 -0800

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - write a function that prints the name
+ * of a file
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+	printf("%s\n", "_FILE_");
+	return (0);
+}
+
